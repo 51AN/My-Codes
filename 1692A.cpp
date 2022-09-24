78b1ed5697e5c7b6e03c38b1ac474fdc6689d83d
51AN <sianashsad@iut-dhaka.edu>
2022-09-24 23:29:01 +0600

    hehe

---
+++ 1692A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll                                               long long int
+#define ektu_chalak_na_hole_duniya_te_tika_boroi_kothin  (ios::sync_with_stdio(0),cin.tie(0));
+#define ekdin_to_ferot_jetei_hobe                        return 0;
+#define nl                                                "\n"
+#define f(i,n)                                           for(int i = 0 ; i < n ; i ++)
+#define f1(i,n)                                          for(int i = 1 ; i <= n ; i ++)
+#define fb(i,n)                                          for(int i = n - 1 ; i => 0 ; i --)
+#define v(i)                                             vector<int>v[i];
+#define sortv(a)                                         sort(a.begin(),a.end())
+#define test(t)                                          ll t; cin>>t; while(t--)
+
+using namespace std;
+
+
+
+int main()
+{
+    ektu_chalak_na_hole_duniya_te_tika_boroi_kothin;
+    
+    test(t){
+
+        ll a, b, c, d;
+        cin>>a>>b>>c>>d;
+
+        ll count = 0;
+
+        if(a<b){
+            count++;
+        }
+        if(a<c){
+            count++;
+        }
+        if(a<d){
+            count++;
+        }
+
+        cout<<count<<nl;
+        
+    }
+
+    
+
+
+    ekdin_to_ferot_jetei_hobe;
+}
