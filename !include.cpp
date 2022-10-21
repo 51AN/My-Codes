6be57317b094a70b136e2e67993e55896dd189d6
51AN <sianashsad@iut-dhaka.edu>
2022-10-21 06:41:54 +0600

    don't know why there's an update

--- !include.cpp
+++ !include.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #define ll                                               long long int
-#define fastIO                                           (ios::sync_with_stdio(0),cin.tie(0));
-#define r0                                               return 0;
+#define fastIO  (ios::sync_with_stdio(0),cin.tie(0));
+#define re                        return 0;
 #define nl                                                "\n"
 #define f(i,n)                                           for(int i = 0 ; i < n ; i ++)
 #define f1(i,n)                                          for(int i = 1 ; i <= n ; i ++)
@@ -17,25 +17,83 @@ using namespace std;
 
 
 
-int main()
-{
-    fastIO;
-    
-    // test(t){
+    priority_queue<int, vector<int>, greater<int> >upper;
+    priority_queue<int>lower;
+
+
+    void AddNum(int x){
+
+        if(upper.size() == 0  && lower.size() == 0){
+            upper.push(x);
+        }
+        else
+     {    if(x>=upper.top()){
+            upper.push(x);
+        }
+        else{
+            lower.push(x);
+        }
+    }
+        if(upper.size() - lower.size() > 1){
+            int temp = 0;
+            temp = upper.top();
+            lower.push(temp);
+            upper.pop();
+        }
+        else if(lower.size() - upper.size() > 1){
+            int temp = 0;
+            temp = lower.top();
+            upper.push(temp);
+            lower.pop();
+        }
+
+
         
-    // }
-
-    string s = "abce";
-    string t = "abcd";
-
-    if(s<t){
-        YES;
     }
-    else{
-        NO;
+
+    double FindMedian(){
+        double median = 0;
+        if(lower.size()>upper.size()){
+            median = (double)lower.top();
+        }
+        else if(lower.size()<upper.size()){
+            median = (double)upper.top();
+        }
+        else{
+            median = (double)(((double)lower.top() + (double)upper.top())/(double)2.0);
+        }
+        return median;
     }
+
+
+
+int main(){
+
+    fastIO;
+
     
 
+    AddNum(10);
+    AddNum(12);
+    AddNum(9);
+    // cout<<FindMedian()<<nl;
+    AddNum(8);
+    // cout<<FindMedian()<<nl;
 
-    r0;
+    cout<<lower.size()<<nl;
+    cout<<upper.size()<<nl;
+
+    while(lower.size()!=0){
+        cout<<lower.top()<<nl;
+        lower.pop();
+    }
+    while(upper.size()!=0){
+        cout<<upper.top()<<nl;
+        upper.pop();
+    }
+
+
+
+    re;
+
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,75 +1,13 @@
-#include<bits/stdc++.h>
-#define ll                                               long long int
-#define ektu_chalak_na_hole_duniya_te_tika_boroi_kothin  (ios::sync_with_stdio(0),cin.tie(0));
-#define ekdin_to_ferot_jetei_hobe                        return 0;
-#define nl                                                "\n"
-using namespace std;
-int main()
-{
-
-    ektu_chalak_na_hole_duniya_te_tika_boroi_kothin;
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        ll n,m,rowb,colb,rowc,colc;
-        bool rowplus = true;
-        bool colplus = true;
-        cin>>n>>m>>rowb>>colb>>rowc>>colc;
-        ll cnt = 0;
-        for(int i=rowb,j=colb;;)
-        {
-            if(i==rowc || j==colc)
-            {
-                cout<<cnt/2<<nl;
-                break;
-            }
-            if(i==n)
-            {
-                rowplus=false;
-            }
-            if(j==m)
-            {
-                colplus = false;
-            }
-            if(i==0)
-            {
-                rowplus=true;
-            }
-            if(j==0)
-            {
-                colplus=true;
-            }
-
-           if(rowplus)
-           {
-               ++i;
-               ++cnt;
-
-           }
-           else
-           {
-               --i;
-               ++cnt;
-           }
-           if(colplus)
-           {
-               ++j;
-               ++cnt;
-           }
-           else
-           {
-               --j;
-               ++cnt;
-           }
-        }
-
-
-            
-
-        
-
-        
-    }
-    ekdin_to_ferot_jetei_hobe;
-}
+double FindMedian(){
+    //     double median = 0;
+    //     if(lower.size()>upper.size()){
+    //         median = (double)lower.top();
+    //     }
+    //     else if(lower.size()<upper.size()){
+    //         median = (double)upper.top();
+    //     }
+    //     else{
+    //         median = (double)(((double)lower.top() + (double)upper.top())/(double)2.0);
+    //     }
+    //     return median;
+    // }
