816d90c0115d079761b5811bd77036a601ebf29a
51AN <sianashsad@iut-dhaka.edu>
2022-11-14 23:15:07 +0600

    OOC code refactor

--- !include.cpp
+++ !include.cpp
@@ -18,21 +18,48 @@ using namespace std;
 int main(){
 
     fastIO;
-    bool y=false;
-    int x = 0;
-    if(true)
-        x=1;
+    
+    int month;
+    int year;
+    int day;
 
-    if(true)
-    {    x=2;
-    if(true)
-        x=3;
-    else
-        x=-1;}
-    else
-        x=-2;
-
-
+    if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) && day == 31) 
+    {
+        day = 1;   month++;
+    } 
+    else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 30) 
+    {
+        day = 1;   month++;
+    } 
+    else if ((month == 12) && day == 31) 
+    {
+        day = 1;   month++;   year++;
+    } 
+    else if ((month == 2)) 
+    {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) 
+        {
+            if (day == 29) 
+            {
+                day = 1;   month++;
+            } 
+            else 
+            {
+                day = day + 1;
+            }
+        } 
+        else 
+        {
+            if (day == 28) 
+            {
+                day = 1; month++;
+            }
+        }
+    } 
+    else 
+    {
+        day = day + 1;
+    }
 
     re;
 
